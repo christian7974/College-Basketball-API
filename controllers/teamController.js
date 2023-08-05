aa61aed58bd421d54b14fed7f70892d4d5fc0211
christian7974 <ctropeano@uri.edu>
2023-08-05 16:45:01 -0400

    Can Find All Teams, Team by Name
    Implemented functionality to find a team by its name or find every team.

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,6 +1,7 @@
 const Team = require('../models/teamModel');
 const asyncHandler = require('express-async-handler');
 
+// Creates a team in the Database (used for initialization)
 const createTeam = asyncHandler(async(req, res) => {
     try {
         const individualTeam = await Team.create(req.body);
@@ -10,4 +11,35 @@ const createTeam = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports = createTeam;
+// Shows every team and their stats in the database
+const showAllTeams = asyncHandler(async(req, res) => {
+    try {
+        const allTeams = await Team.find({});
+        res.status(200).json(allTeams);
+    } catch (error) {
+        res.status(500);
+    }
+});
+// Function that turns a string into titlecasse (that is how the team names are stored in the database)
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+
+// Function that can find a team by the name passed in as a parameter
+const findTeamByName = asyncHandler(async(req, res) => {
+    try {
+        const {teamName} = req.params;
+        const properTeamName = titleCase(teamName);
+        console.log(properTeamName);
+        const theTeam = await Team.find({"name": properTeamName});
+        res.status(200).json(theTeam);
+    } catch (error) {
+        res.status(500);
+    }
+});
+
+module.exports = {createTeam, showAllTeams, findTeamByName};
--- routes/teamRoute.js
+++ routes/teamRoute.js
@@ -4,9 +4,13 @@ const Team = require('../models/teamModel');
 
 const router = express.Router();
 
-const createTeam = require('../controllers/teamController');
+const {createTeam, showAllTeams, findTeamByName} = require('../controllers/teamController');
 
 // Add Team
 router.post('/', createTeam);
+
+router.get('/all', showAllTeams);
+
+router.get('/:teamName', findTeamByName);
 
 module.exports = router;
