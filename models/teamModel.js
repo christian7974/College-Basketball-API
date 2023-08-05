ff2feb6ed69d430fee6581f7cb043e3ac0272c7c
christian7974 <ctropeano@uri.edu>
2023-08-05 13:56:10 -0400

    Initialized Schema, Created Controller and Route Folders
    More initialization, installation of modules. Created schema for each team.

---
+++ models/teamModel.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+const teamSchema = mongoose.Schema({
+    // team name
+    name: {
+        type: String,
+        required: [true, "Every team must have a name"]
+    },
+    // points per game
+    pointsPG: {
+        type: Number
+    },
+    // field goals made per game
+    fieldGoalsMadePG: {
+        type: Number
+    },
+    // field goal attempts per game
+    fieldGoalsAttPG: {
+        type: Number
+    },
+    // field goal percent per game
+    FGPercent: {
+        type: Number
+    },
+    // 3 point shots made per game
+    threePointMadePG: {
+        type: Number
+    },
+    // 3 point shots attempted per game
+    threePointAttPG: {
+        type: Number
+    },
+    // 3 point percentage
+    threePointPercent: {
+        type: Number
+    },
+    // free throws made per game
+    freeThrowMadePG: {
+        type: Number
+    },
+    // free throws attempted per game
+    freeThrowAttPG: {
+        type: Number
+    },
+    // free throw percentage
+    freeeThrowPercent: {
+        type: Number
+    },
+    // offensive rebounds per game
+    offReboundsPG: {
+        type: Number
+    },
+    // defensive rebounds per game
+    defReboundsPG: {
+        type: Number
+    },
+    // total rebounds per game
+    totalReboundsPG: {
+        type: Number
+    },
+    // assists per game
+    assistsPG: {
+        type: Number
+    },
+    // steals per game
+    stealsPG: {
+        type: Number
+    },
+    // blocks per game
+    blocksPG: {
+        type: Number
+    },
+    // turnovers committed per game
+    turnoversPG: {
+        type: Number
+    },
+});
+
+const individualTeam = mongoose.model('Team', teamSchema);
+
+module.exports = individualTeam;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send("Welcome to my College Basketball API");
 });
 
-app.listen(3000, () => {
-    console.log("The server is running on port 3000!");
+mongoose
+.connect(MONGO_URL)
+.then(() => {
+    console.log("connected to mongodb");
+    app.listen(PORT, () => {
+        console.log(`Node API app is running on Port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 });
